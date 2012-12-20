0a39399738c2b8bb7a7489352da5dcf8fc32d1dc
Meg4mi <meg4mi@gmail.com>
2012-12-20 19:01:49 +0100

    ça marche et vous avez la conf sur asana ^^

--- GAC-web/src/managedBean/ProjetMB.java
+++ GAC-web/src/managedBean/ProjetMB.java
@@ -1,15 +1,26 @@
 package managedBean;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import remote.FProjectServicesRemote;
 
 
 @ManagedBean
 @SessionScoped
 public class ProjetMB {
+	
+	
+	 @EJB
+	 private FProjectServicesRemote fps;
 
 	public ProjetMB() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public String getName(){
+		return fps.findItem(1).getName();
+	}
+	
 }
