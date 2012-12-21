5e4dc0201eda6855617baa2e339b9ccf4663ce7d
remimarenco <remi.marenco@gmail.com>
2012-12-21 15:32:58 +0100

    Test ok pour workflow complet appli

--- GAC-EJBSession/ejbModule/local/ProjectServiceLocal.java
+++ GAC-EJBSession/ejbModule/local/ProjectServiceLocal.java
@@ -1,5 +1,7 @@
 package local;
 
+import java.util.Collection;
+
 import javax.ejb.Local;
 
 import model.Project;
@@ -7,12 +9,14 @@ import model.Project;
 @Local
 public interface ProjectServiceLocal {
 
-	  public void addItem(Project i);
+	  	public void addItem(Project i);
 
 	    public Project findItem(Integer id);
 
 	    public void deleteItem(Project i);
 
 	    public void updateItem(Project i);
+	    
+	    public Collection<Project> findAllProject();
 	
 }
