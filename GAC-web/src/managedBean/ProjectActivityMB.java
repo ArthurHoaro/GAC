0e7ef3fcafd2d73700f8b6aa07b92b16e2aefa7b
Hedi Tarchouni <hedi.tarchouni@gmail.com>
2012-12-23 16:44:22 +0100

    update pour l'ajout des heures dans une activité (projectActivity.xhtml) manque design + recuperation des parametres+ historiques des actions

--- GAC-web/src/managedBean/ProjectActivityMB.java
+++ GAC-web/src/managedBean/ProjectActivityMB.java
@@ -5,28 +5,44 @@ package managedBean;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.NoResultException;
 
 
 import remote.FActivityServicesRemote;
 import remote.FProjectServicesRemote;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @ManagedBean
 @SessionScoped
 public class ProjectActivityMB {
 	
+	// Properties ---------------------------------------------------------------------------------
 	
 	 @EJB
 	 private FActivityServicesRemote fas;
-	 
-	 
-		private int idActivity;
-		private int idEmployee;
-		private int chargeAAjouter=2;
+ 
+	private int idActivity;
+	private int idEmployee;
+	private int chargeAAjouter=0;
 
+	// Actions ------------------------------------------------------------------------------------
+	
 	public ProjectActivityMB() {
-		//on recupere les parametres
-		idActivity=10;
+		
+		//on recupere l' id de l'activité passé par l url 
+		if(true) {
+			idActivity=10;
+		} else {
+			idActivity=0;
+		}
+		
+		//on recupere l'id de l'employe connecté
 		idEmployee=1;
 
 	}
